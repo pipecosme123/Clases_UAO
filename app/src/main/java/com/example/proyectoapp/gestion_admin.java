47f55efb79d9d107c634b424ce8f3888556d3939
Usuario ILINF <admin-ilinf@uao.edu.co>
2022-04-02 16:51:02 -0500

    Base del proyecto

---
+++ app/src/main/java/com/example/proyectoapp/gestion_admin.java
@@ -0,0 +1,14 @@
+package com.example.proyectoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gestion_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_admin);
+    }
+}
