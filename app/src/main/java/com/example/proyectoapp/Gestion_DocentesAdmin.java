08da846d69334b1d11709418f324b0e71bb3fe2d
Juan Camilo Bolanos <juan_cam.bolanos@uao.edu.co>
2022-04-08 13:47:32 -0500

    pantalla de gestion admin

---
+++ app/src/main/java/com/example/proyectoapp/Gestion_DocentesAdmin.java
@@ -0,0 +1,14 @@
+package com.example.proyectoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gestion_DocentesAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_docentes_admin);
+    }
+}
--- app/src/main/java/com/example/proyectoapp/gestion_admin.java
+++ app/src/main/java/com/example/proyectoapp/gestion_admin.java
@@ -3,12 +3,32 @@ package com.example.proyectoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+import android.content.Intent;
 
 public class gestion_admin extends AppCompatActivity {
 
+    private Button btCursos, btDocentes, btAlumnos, btNovedades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion_admin);
+
+        btCursos = (Button) findViewById(R.id.gestionAdmin_btCursos);
+        btDocentes = (Button) findViewById(R.id.gestionAdmin_btDocentes);
+        btAlumnos = (Button) findViewById(R.id.gestionAdmin_btEstudaintes);
+        btNovedades = (Button) findViewById(R.id.gestionAdmin_Novedades);
+    }
+
+    public void irACursos(View view){
+        Intent i = new Intent(gestion_admin.this, gestion_cursosAdmin.class);
+        startActivity(i);
+    }
+
+    public void irADocentes(View view){
+        Intent i = new Intent(gestion_admin.this, Gestion_DocentesAdmin.class);
+        startActivity(i);
     }
 }   
