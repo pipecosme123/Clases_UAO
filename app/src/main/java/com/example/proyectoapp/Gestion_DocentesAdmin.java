800375deb70170c5e78441379332be8d0c0bd8a7
Juan Camilo Bolanos <juan_cam.bolanos@uao.edu.co>
2022-04-08 14:46:38 -0500

    creando actividad docentes admin

--- app/src/main/java/com/example/proyectoapp/Gestion_DocentesAdmin.java
+++ app/src/main/java/com/example/proyectoapp/Gestion_DocentesAdmin.java
@@ -3,9 +3,11 @@ package com.example.proyectoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Gestion_DocentesAdmin extends AppCompatActivity {
 
+    private Button btCualquierCOsa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
